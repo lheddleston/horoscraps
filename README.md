b092ec3355c34333ee9cd8f3582dd4a55e2f3723
Laura Heddleston <laura.heddleston@gmail.com>
2018-06-14 14:04:22 -0700

    initial commit: react app setup

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# horoscraps
-Where you'll learn how to live your life correctly
+# Horoscraps
+
+### We watch the moon cycles and tell you how to live your life correctly!
+
+It's a React App:
+npm / yarn install
+npm / yarn start
+
+[Here](https://github.com/facebookincubator/create-react-app/blob/master/packages/react-scripts/template/README.md)
+is the React App guide for reference.
---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      <header className="App-header">
+      <h1 className="App-title">Welcome to Horoscraps!</h1>
+      </header>
+      <p>
+      Ares, you’ll be fired from Forever 21 because Brian ratted you out.
+      </p>
+      <p>
+      Don’t wear flip flops on Tuesdays, a pile of shit is headed your way…literally.
+      </p>
+      </div>
+    );
+  }
+}
+
+export default App;
