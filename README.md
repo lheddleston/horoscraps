cb115b043e812fc1aebca3fd4d1fe3ee52106155
Laura Heddleston <laura.heddleston@gmail.com>
2018-08-31 17:11:35 -0700

    feat: rough draft of user form

--- README.md
+++ README.md
@@ -2,9 +2,10 @@
 
 ### We watch the moon cycles and tell you how to live your life correctly!
 
+
 It's a React App:
-npm / yarn install
-npm / yarn start
+1. npm / yarn install <br>
+2. npm / yarn start
 
 [Here](https://github.com/facebookincubator/create-react-app/blob/master/packages/react-scripts/template/README.md)
 is the React App guide for reference.
--- src/App.js
+++ src/App.js
@@ -8,15 +8,30 @@ class App extends Component {
       <header className="App-header">
       <h1 className="App-title">Welcome to Horoscraps!</h1>
       </header>
-      <p>
-      Ares, you’ll be fired from Forever 21 because Brian ratted you out.
-      </p>
-      <p>
-      Don’t wear flip flops on Tuesdays, a pile of shit is headed your way…literally.
-      </p>
+      <form name="userInfo">
+      <ul>
+      <li>
+      Your name:
+      <input type="text" name="firstname"/>
+      </li>
+      <li>
+      Birth month:
+      <input type="text" name="birthmonth"/>
+      </li>
+      <li>
+      Birth date:
+      <input type="text" name="birthdate"/>
+      </li >
+      <input type="submit" value="Send into the universe"/>
+      </ul>
+      </form>
       </div>
     );
   }
 }
+// Responses
+//    Ares, you’ll be fired from Forever 21 because Brian ratted you out.
+//    Don’t wear flip flops on Tuesdays, a pile of shit is headed your way…literally.
+
 
 export default App;
